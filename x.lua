853d47f4e5df9957ce4a48f3438b1b9b62298f60
d3ltaX <121907834+d3ltaX@users.noreply.github.com>
2023-03-10 17:52:10 -0600

    Update x.lua

--- x.lua
+++ x.lua
@@ -1,15 +1,8 @@
 local RBXScriptSignal = {}
 local VRBX = nil
 local FOLDER = Instance.new("Folder", workspace)
-function r(n)
-    local min, max, final = ("a"):byte(), (")"):byte(), ""
 
-    for i = 1, n do
-        final ..= string.char(math.random(min, max))
-    end
-    return final
-end
-FOLDER.Name = r(10)
+FOLDER.Name = "VRBXSiGNALINNN"
 function RBXScriptSignal.new()
 VRBX = Instance.new("BindableEvent", FOLDER)
 return VRBX
